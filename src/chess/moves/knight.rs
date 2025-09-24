e070d858f39aa63767d8e979e60c8af79dacc2f6
Frigge <frigerioalessandro659@gmail.com>
2025-09-24 17:56:16 +0200

    add knight movement and attack functions with bit manipulation

--- src/chess/moves/knight.rs
+++ src/chess/moves/knight.rs
@@ -3,11 +3,35 @@ const NOT_HFILE: u64 = 0x7f7f7f7f7f7f7f7f;
 const NOT_ABFILE: u64 = 0xfcfcfcfcfcfcfcfc;
 const NOT_GHFILE: u64 = 0x3f3f3f3f3f3f3f3f;
 
-pub fn north_northWest(b :u64) -> u64 {(b << 15) & NOT_HFILE}
-pub fn north_northEast(b :u64) -> u64 {(b << 17) & NOT_AFILE}
-pub fn north_westWest(b :u64) -> u64 {(b << 6) & NOT_GHFILE}
-pub fn north_eastEast(b :u64) -> u64 {(b << 10) & NOT_ABFILE}
-pub fn south_westWest(b : u64) -> u64 {(b >> 10) & NOT_GHFILE}
-pub fn south_eastEast(b: u64) -> u64 {(b >> 6) & NOT_ABFILE}
-pub fn south_southWest(b: u64) -> u64 {(b >> 17) & NOT_HFILE}
-pub fn south_southEast(b :u64) -> u64 {(b >> 15) & NOT_AFILE}
+pub fn moves(b : u64, empty : u64) -> u64 {
+    (north_north_west(b) |
+        north_north_east(b) |
+        north_west_west(b) |
+        north_east_east(b) |
+        south_west_west(b) |
+        south_east_east(b) |
+        south_south_west(b) |
+        south_south_east(b)
+    ) & empty
+}
+
+pub fn attack(b : u64, occ : u64) -> u64 {
+    (north_north_west(b) |
+        north_north_east(b) |
+        north_west_west(b) |
+        north_east_east(b) |
+        south_west_west(b) |
+        south_east_east(b) |
+        south_south_west(b) |
+        south_south_east(b)
+    ) & occ
+}
+
+fn north_north_west(b :u64) -> u64 {(b << 15) & NOT_HFILE}
+fn north_north_east(b :u64) -> u64 {(b << 17) & NOT_AFILE}
+fn north_west_west(b :u64) -> u64 {(b << 6) & NOT_GHFILE}
+fn north_east_east(b :u64) -> u64 {(b << 10) & NOT_ABFILE}
+fn south_west_west(b : u64) -> u64 {(b >> 10) & NOT_GHFILE}
+fn south_east_east(b: u64) -> u64 {(b >> 6) & NOT_ABFILE}
+fn south_south_west(b: u64) -> u64 {(b >> 17) & NOT_HFILE}
+fn south_south_east(b :u64) -> u64 {(b >> 15) & NOT_AFILE}
